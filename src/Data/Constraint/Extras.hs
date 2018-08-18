2e5872b0a07f893ff217c02879ddff43f80e6595
Cale Gibbard <cgibbard@gmail.com>
2018-08-18 09:24:08 -0400

    Add 'has' and 'whichever' functions. Generalise the kind of ConstraintsFor.

--- src/Data/Constraint/Extras.hs
+++ src/Data/Constraint/Extras.hs
@@ -2,17 +2,29 @@
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE ConstraintKinds #-}
+{-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE AllowAmbiguousTypes #-}
 
 module Data.Constraint.Extras where
 
 import Data.Constraint
+import Data.Constraint.Forall
 
 -- | Provides proof of the existence of a constraint on a GADT
 class ArgDict f where
-  type ConstraintsFor (c :: * -> Constraint) f :: Constraint
+  type ConstraintsFor (c :: k -> Constraint) f :: Constraint
   argDict :: ConstraintsFor c f => f a -> Dict (c a)
 
 type Has c f = (ArgDict f, ConstraintsFor c f)
+
+has :: forall c f a r. (Has c f) => f a -> (c a => r) -> r
+has k r | (Dict :: Dict (c a)) <- argDict k = r
+
+whichever :: forall c t a r. (ForallF c t) => (c (t a) => r) -> r
+whichever r = r \\ (instF :: ForallF c t :- c (t a))
 
 -- | Allows explicit specification of constraint implication
 class Implies1 c d where
